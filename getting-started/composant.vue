1ef1405a2006f30a1773e7e3f9c888ae8eafced5
Laurent Gourouvin <gourouvin.laurent@gmail.com>
2023-02-25 18:18:44 +0100

    Exemple d'un composant Vue

---
+++ getting-started/composant.vue
@@ -0,0 +1,10 @@
+<!--Voici un à quoi ressemble un simple composant Vue-->
+Vue.component('button-counter', {
+<!--Data doit être une fonction-->
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
