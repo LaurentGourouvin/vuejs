2d089acd10eac66c51d8c72c242ebb67f8cb98e4
Laurent Gourouvin <gourouvin.laurent@gmail.com>
2023-03-11 10:39:06 +0100

    Update: Refacto du code et créer d'un composant todo-list

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -13,18 +13,14 @@
         </div>
         <v-divider></v-divider>
 
-        <v-toolbar title="Tâche en cours" elevation="1" color="#FFF176">Tâche en cours</v-toolbar>
-        <todos-list color="#FFF9C4" :todos="todosInProgress"></todos-list>
-        <v-toolbar title="Tâche terminées" elevation="1" color="#66BB6A">Tâche terminées</v-toolbar>
-        <todos-list color="#A5D6A7" :todos="todosDone"></todos-list>
+        <todos-list :todos="todos"></todos-list>
       </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-import { TODOS } from "./data";
-import _ from "lodash";
+import {TODOS} from "./data";
 // Import components
 import TodosList from "./components/TodosList";
 
@@ -35,27 +31,17 @@ export default {
   },
   data: () => ({
     todos: TODOS,
-    todosInProgress: null,
-    todosDone: null,
     newTodo: "",
   }),
   methods: {
     addTodos() {
-      this.todosInProgress.push({ id: 0, text: this.newTodo, status: true });
+      this.todos.push({id: 0, text: this.newTodo, status: true});
       this.newTodo = "";
-    },
-    getTodosInProgress() {
-      const todosInProgress = _.cloneDeep(this.todos);
-      return todosInProgress.filter((todos) => todos.status === true);
-    },
-    getTodosDone() {
-      const todosDone = _.cloneDeep(this.todos);
-      return todosDone.filter((todos) => todos.status === false);
-    },
+    }
   },
+
   mounted() {
-    this.todosInProgress = this.getTodosInProgress();
-    this.todosDone = this.getTodosDone();
+
   },
 };
 </script>
@@ -68,13 +54,16 @@ export default {
   align-items: center;
   background-color: cadetblue;
 }
+
 #container {
   width: 600px;
 }
+
 #card {
   min-height: 500px;
   width: 100%;
 }
+
 #card_flex {
   display: flex;
   flex-direction: column;
@@ -82,15 +71,18 @@ export default {
   width: 85%;
   gap: 0.75rem;
 }
+
 #card_flex--title {
   font-size: 24px;
   font-weight: bold;
   font-style: italic;
 }
+
 #form {
   display: flex;
   flex-direction: row;
 }
+
 #formulaire {
   display: flex;
   align-items: center;
