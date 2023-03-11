207bb1396db2e0e6316ef9ec41060fb710e52216
Laurent Gourouvin <gourouvin.laurent@gmail.com>
2023-03-11 10:38:23 +0100

    UPDATE: mise à jour des datas avec l'ajout de la propriété createdAt

--- todolist/src/data.js
+++ todolist/src/data.js
@@ -3,40 +3,48 @@ export const TODOS = [
     id: 0,
     text: "Apprendre VueJS",
     status: true,
+    createdAt: new Date(),
   },
   {
     id: 1,
     text: "Apprendre Vuex",
     status: true,
+    createdAt: new Date(),
   },
   {
     id: 2,
     text: "Apprendre Vue Router",
     status: true,
+    createdAt: new Date(),
   },
   {
     id: 3,
     text: "Apprendre Vue CLI",
     status: true,
+    createdAt: new Date(),
   },
   {
     id: 4,
     text: "Apprendre Vue Event Bus",
     status: true,
+    createdAt: new Date(),
   },
   {
     id: 5,
     text: "Apprendre SASS",
     status: true,
+    createdAt: new Date(),
   },
   {
     id: 6,
     text: "Apprendre React",
     status: false,
+    createdAt: new Date(),
   },
   {
     id: 7,
     text: "Apprendre NextJS",
     status: false,
+    createdAt: new Date(),
   },
 ];
